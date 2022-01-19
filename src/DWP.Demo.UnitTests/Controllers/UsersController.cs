c583c760f00a7f10cdaf4adbb6579930eb420c4d
Martin Poulton <mapoulton@gmail.com>
2022-01-19 20:00:38 +0000

    Squashed commit of the following:
    commit 9a99dd5f1924550e57f2c2c0b6030d79fc518ce6
    Author: Martin Poulton <mapoulton@gmail.com>
    Date:   Wed Jan 19 19:58:31 2022 +0000
    
        Added Readme with notes
    
    commit 7c343d970283382b5267e6021fd0112ac656704b
    Author: Martin Poulton <mapoulton@gmail.com>
    Date:   Wed Jan 19 19:41:44 2022 +0000
    
        Get With Duplicate Users - Passing Test
    
    commit 59500bae74e650f20e3f77b15c3ab93552bc4e36
    Author: Martin Poulton <mapoulton@gmail.com>
    Date:   Wed Jan 19 19:40:18 2022 +0000
    
        Get With Duplicate Users - Failing Test
    
    commit bb75348cf2b1d6e34a471b2a646e92f350987794
    Author: Martin Poulton <mapoulton@gmail.com>
    Date:   Wed Jan 19 19:38:27 2022 +0000
    
        Get Users When User Removed - Passing Test
    
    commit 96ad91fc1585f4e7d21b5dbefc6d5f1e95e64b9b
    Author: Martin Poulton <mapoulton@gmail.com>
    Date:   Wed Jan 19 19:36:05 2022 +0000
    
        Get Users Controller - Passing Test
    
    commit 96943363c5e1d3e73f0a4f70475098a0a91a5ca0
    Author: Martin Poulton <mapoulton@gmail.com>
    Date:   Wed Jan 19 19:31:02 2022 +0000
    
        Get Users Controller - Failing Test

---
+++ src/DWP.Demo.UnitTests/Controllers/UsersController.cs
@@ -0,0 +1,184 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DWP.Demo.Api.Domain.Filters;
+using DWP.Demo.Api.Domain.HttpClient;
+using DWP.Demo.Api.Types;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+
+namespace DWP.Demo.UnitTests.Controllers
+{
+    [TestFixture]
+    public class UsersController
+    {
+        [Test]
+        public async Task Get_HappyPath()
+        {
+            const string expectedCity = "London";
+            const double expectedDistance = 50.0;
+            const double expectedLatitude = 51.509865;
+            const double expectedLongitude = -0.118092;
+
+            const int user1Id = 1;
+            const int user2Id = 2;
+            
+            var getUsers = new[] { new User() { Id = user1Id } };
+            var cityUsers = new[] { new User() { Id = user2Id } };
+            var filteredResults = cityUsers;
+            
+            var getUsersQuery = new Mock<IGetUsers>();
+            getUsersQuery.Setup(x => x.Execute())
+                .ReturnsAsync(getUsers);
+            
+            var getUsersByCity = new Mock<IGetUsersByCity>();
+            getUsersByCity.Setup(x => x.Execute(expectedCity))
+                .ReturnsAsync(cityUsers);
+            
+            var userFilter = new Mock<IUserDistanceFilter>();
+            userFilter.Setup(x =>
+                    x.RemoveUsersWithDistanceGreaterThan(cityUsers, expectedLatitude, expectedLongitude,
+                        expectedDistance))
+                .Returns(filteredResults);
+            
+            var sut = new UserController(getUsersQuery.Object, getUsersByCity.Object, userFilter.Object);
+
+            var result = await sut.Get();
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<OkObjectResult>());
+            var okObjectResult = result as OkObjectResult;
+            
+            Assert.That(okObjectResult.Value, Is.InstanceOf<IEnumerable<User>>());
+            var users = okObjectResult.Value as IEnumerable<User>;
+
+            Assert.That(users.Count(), Is.EqualTo(2));
+            Assert.That(users, Has.Exactly(1).Property(nameof(User.Id)).EqualTo(user1Id));
+            Assert.That(users, Has.Exactly(1).Property(nameof(User.Id)).EqualTo(user2Id));
+        }
+
+        [Test]
+        public async Task Get_UsersRemovedDueToDistance()
+        {
+            const string expectedCity = "London";
+            const double expectedDistance = 50.0;
+            const double expectedLatitude = 51.509865;
+            const double expectedLongitude = -0.118092;
+
+            const int user1Id = 1;
+            const int user2Id = 2;
+            const int user3Id = 3;
+            
+            var getUsers = new[] { new User() { Id = user1Id } };
+            var cityUsers = new[] { new User() { Id = user2Id }, new User() { Id = user3Id } };
+            var filteredResults = new[] { new User() { Id = user3Id } };
+            
+            var getUsersQuery = new Mock<IGetUsers>();
+            getUsersQuery.Setup(x => x.Execute())
+                .ReturnsAsync(getUsers);
+            
+            var getUsersByCity = new Mock<IGetUsersByCity>();
+            getUsersByCity.Setup(x => x.Execute(expectedCity))
+                .ReturnsAsync(cityUsers);
+            
+            var userFilter = new Mock<IUserDistanceFilter>();
+            userFilter.Setup(x =>
+                    x.RemoveUsersWithDistanceGreaterThan(cityUsers, expectedLatitude, expectedLongitude,
+                        expectedDistance))
+                .Returns(filteredResults);
+            
+            var sut = new UserController(getUsersQuery.Object, getUsersByCity.Object, userFilter.Object);
+
+            var result = await sut.Get();
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<OkObjectResult>());
+            var okObjectResult = result as OkObjectResult;
+            
+            Assert.That(okObjectResult.Value, Is.InstanceOf<IEnumerable<User>>());
+            var users = okObjectResult.Value as IEnumerable<User>;
+
+            Assert.That(users.Count(), Is.EqualTo(2));
+            Assert.That(users, Has.Exactly(1).Property(nameof(User.Id)).EqualTo(user1Id));
+            Assert.That(users, Has.Exactly(1).Property(nameof(User.Id)).EqualTo(user3Id));
+        }
+        
+        [Test]
+        public async Task Get_DuplicateUsers_HasRemovesDuplicates()
+        {
+            const string expectedCity = "London";
+            const double expectedDistance = 50.0;
+            const double expectedLatitude = 51.509865;
+            const double expectedLongitude = -0.118092;
+
+            const int user1Id = 1;
+            
+            var getUsers = new[] { new User() { Id = user1Id } };
+            var cityUsers = new[] { new User() { Id = user1Id } };
+            var filteredResults = new[] { new User() { Id = user1Id } };
+            
+            var getUsersQuery = new Mock<IGetUsers>();
+            getUsersQuery.Setup(x => x.Execute())
+                .ReturnsAsync(getUsers);
+            
+            var getUsersByCity = new Mock<IGetUsersByCity>();
+            getUsersByCity.Setup(x => x.Execute(expectedCity))
+                .ReturnsAsync(cityUsers);
+            
+            var userFilter = new Mock<IUserDistanceFilter>();
+            userFilter.Setup(x =>
+                    x.RemoveUsersWithDistanceGreaterThan(cityUsers, expectedLatitude, expectedLongitude,
+                        expectedDistance))
+                .Returns(filteredResults);
+            
+            var sut = new UserController(getUsersQuery.Object, getUsersByCity.Object, userFilter.Object);
+
+            var result = await sut.Get();
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result, Is.InstanceOf<OkObjectResult>());
+            var okObjectResult = result as OkObjectResult;
+            
+            Assert.That(okObjectResult.Value, Is.InstanceOf<IEnumerable<User>>());
+            var users = okObjectResult.Value as IEnumerable<User>;
+
+            Assert.That(users.Count(), Is.EqualTo(1));
+            Assert.That(users, Has.Exactly(1).Property(nameof(User.Id)).EqualTo(user1Id));
+        }
+    }
+
+    public class UserController : Controller
+    {
+        private readonly IGetUsers _getUsers;
+        private readonly IGetUsersByCity _getUsersByCity;
+        private readonly IUserDistanceFilter _userDistanceFilter;
+
+        private const string City = "London";
+        private const double LondonLatitude = 51.509865;
+        private const double LondonLongitude = -0.118092;
+        private const double MaxDistance = 50.0;
+
+        public UserController(IGetUsers getUsers, IGetUsersByCity getUsersByCity, IUserDistanceFilter userDistanceFilter)
+        {
+            _getUsers = getUsers;
+            _getUsersByCity = getUsersByCity;
+            _userDistanceFilter = userDistanceFilter;
+        }
+
+        public async Task<IActionResult> Get()
+        {
+            var getUsersTask = _getUsers.Execute();
+            var getUsersByCityTask = _getUsersByCity.Execute(City);
+
+            await Task.WhenAll(getUsersTask, getUsersByCityTask);
+
+            var removedUsers = _userDistanceFilter.RemoveUsersWithDistanceGreaterThan(
+                getUsersByCityTask.Result, LondonLatitude, LondonLongitude, MaxDistance);
+
+            var allUsers = getUsersTask.Result.Concat(removedUsers).Distinct();
+
+            return Ok(allUsers);
+        }
+    }
+}
