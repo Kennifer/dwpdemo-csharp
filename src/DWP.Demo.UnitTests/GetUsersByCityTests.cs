e0e1b3209549252321387c50a389b46d9ccc1b52
Martin Poulton <mapoulton@gmail.com>
2022-01-19 11:23:29 +0000

    Execute_ReturnsCorrectlyPopulatedUsers - Failing Test

--- src/DWP.Demo.UnitTests/GetUsersByCityTests.cs
+++ src/DWP.Demo.UnitTests/GetUsersByCityTests.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
@@ -36,6 +39,68 @@ namespace DWP.Demo.UnitTests
 
             httpProxy.Verify(x => x.GetAsync(expectedRequestAddress));
         }
+
+        [Test]
+        public async Task Execute_ReturnsCorrectlyPopulatedUsers()
+        {
+            const string city = "manchester";
+            const string requestUrl = "/city/manchester/users";
+            const string responseString =
+                @"[
+                {
+                    ""id"": 1,
+                    ""first_name"": ""fn1"",
+                    ""last_name"": ""su1"",
+                    ""email"": ""email@1.com"",
+                    ""ip_address"": ""192.168.0.1"",
+                    ""latitude"": 1.1234567,
+                    ""longitude"": -1.1234567
+                },
+                {
+                    ""id"": 2,
+                    ""first_name"": ""fn2"",
+                    ""last_name"": ""su2"",
+                    ""email"": ""email@2.com"",
+                    ""ip_address"": ""192.168.0.2"",
+                    ""latitude"": 2.1234567,
+                    ""longitude"": -2.1234567
+                }
+            ]";
+
+            var httpResponse = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(responseString, Encoding.UTF8, "application/json")
+            };
+            
+            var httpClient = new Mock<IHttpClient>();
+            httpClient.Setup(x => x.GetAsync(requestUrl))
+                .ReturnsAsync(httpResponse);
+
+            var sut = new GetUsersByCity(httpClient.Object);
+
+            var users = await sut.Execute(city);
+
+            Assert.That(users, Is.Not.Null);
+            Assert.That(users.Count(), Is.EqualTo(2));
+
+            var user1 = users.SingleOrDefault(x => x.Id == 1);
+            Assert.That(user1, Is.Not.Null);
+            Assert.That(user1.FirstName, Is.EqualTo("fn1"));
+            Assert.That(user1.LastName, Is.EqualTo("ln1"));
+            Assert.That(user1.Email, Is.EqualTo("email@1.com"));
+            Assert.That(user1.IpAddress, Is.EqualTo("192.168.0.1"));
+            Assert.That(user1.Latitude, Is.EqualTo(1.1234567));
+            Assert.That(user1.Longitude, Is.EqualTo(-1.1234567));
+            
+            var user2 = users.SingleOrDefault(x => x.Id == 2);
+            Assert.That(user2, Is.Not.Null);
+            Assert.That(user2.FirstName, Is.EqualTo("fn2"));
+            Assert.That(user2.LastName, Is.EqualTo("ln2"));
+            Assert.That(user2.Email, Is.EqualTo("email@2.com"));
+            Assert.That(user2.IpAddress, Is.EqualTo("192.168.0.2"));
+            Assert.That(user2.Latitude, Is.EqualTo(2.1234567));
+            Assert.That(user2.Longitude, Is.EqualTo(-2.1234567));
+        }
     }
 
     public interface IGetUsersByCity
@@ -45,11 +110,18 @@ namespace DWP.Demo.UnitTests
 
     public interface IHttpClient
     {
-        Task GetAsync(string url);
+        Task<HttpResponseMessage> GetAsync(string url);
     }
 
-    public class User
+    public record User
     {
+        public int Id { get; init; }
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public string Email { get; init; }
+        public string IpAddress { get; init; }
+        public decimal Latitude { get; init; }
+        public decimal Longitude { get; init; }
     }
 
     public class GetUsersByCity : IGetUsersByCity
